727fcff08ac59fa17c9656e6b9daa5b151e095d0
wilson-romero <me@wilsonromero.com>
2020-06-22 18:39:45 -0500

    Consumir API personalizadas con Next.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
-export default function Index() {
-  return <h1>Hello World</h1>;
+import Container from 'react-bootstrap/Container';
+import fetch from 'isomorphic-fetch';
+import Thoughts from '../components/Thoughts';
+
+function Index(props) {
+  return (
+    <Container>
+      <Thoughts thoughts={props.thoughts} />
+    </Container>
+  );
 }
+
+Index.getInitialProps = async ({ req }) => {
+  const baseURL = req ? `${req.protocol}://${req.get('Host')}` : '';
+  const res = await fetch(`${baseURL}/api/thoughts`);
+  return {
+    thoughts: await res.json(),
+  };
+};
+
+export default Index;
